d7653a76f57a4f73e9607468a56ecbd19dadcd05
Osama Rabea <osrab3@gmail.com>
2024-12-16 03:52:42 +0200

    server

---
+++ index.js
@@ -0,0 +1,54 @@
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+const app = express()
+const cors = require('cors')
+const bookRoutes = require('./routes/books.route')
+const memberRouter = require('./routes/member.route')
+
+app.use(express.json()); 
+app.use(express.static(path.join(__dirname, 'public'))); 
+const verifyToken = require('./middliewares/veryfiyToken')
+
+const memberController = require('./controllers/member.controller')
+
+
+
+
+const corsOptions = {
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+};
+app.use(cors(corsOptions));
+
+
+
+app.use('/api/auth',memberRouter)
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.delete('/api/users/:Id',verifyToken,memberController.deleteUser)
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.use('/api/books',bookRoutes)
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+
+
+
+
+
+
+
+
+const port = process.env.PORT||8080
+app.listen(port,()=>{
+    console.log(`app lisitin in Port ${port}`)
+})
